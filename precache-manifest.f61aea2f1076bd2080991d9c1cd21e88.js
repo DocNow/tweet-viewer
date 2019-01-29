8bb4400de0adb045c064f8b327a3f80fb801b61a
Ed Summers <ehs@pobox.com>
2019-01-29 12:59:11 -0500

    Updates

---
+++ precache-manifest.f61aea2f1076bd2080991d9c1cd21e88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71fd27fed4b747ad755c",
+    "url": "/static/css/main.e17f00cd.chunk.css"
+  },
+  {
+    "revision": "71fd27fed4b747ad755c",
+    "url": "/static/js/main.71fd27fe.chunk.js"
+  },
+  {
+    "revision": "3f0c091d03654427fa68",
+    "url": "/static/js/1.3f0c091d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9a4bfea1439579891f9b2d2562f4752",
+    "url": "/index.html"
+  }
+];
