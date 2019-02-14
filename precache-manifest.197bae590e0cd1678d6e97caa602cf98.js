543f2cbfecbe2076798b55abef47f1a736f99127
raffazizzi <raffaeleviglianti@gmail.com>
2019-02-14 15:38:48 -0500

    new

---
+++ precache-manifest.197bae590e0cd1678d6e97caa602cf98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e2b048a269ed47f94a23",
+    "url": "./static/js/main.e2b048a2.chunk.js"
+  },
+  {
+    "revision": "a89cd99dda77ccc2ecc8",
+    "url": "./static/js/1.a89cd99d.chunk.js"
+  },
+  {
+    "revision": "e2b048a269ed47f94a23",
+    "url": "./static/css/main.2ee247ae.chunk.css"
+  },
+  {
+    "revision": "995b67f0b95d607b4d2b837207d9e865",
+    "url": "./index.html"
+  }
+];
