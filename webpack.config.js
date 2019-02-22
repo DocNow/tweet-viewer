476dd5d1dfa16c8a1bbb31744c01dea9e96d818d
Ed Summers <ehs@pobox.com>
2019-02-22 17:09:12 -0500

    reference the tweet-viewer correctly

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: {
+    "tweet-viewer": './src/index.js',
+  },
   output: {
-    filename: `TweetViewer.js`,
+    filename: `[name].js`,
     path: path.resolve(__dirname, './dist')
   },
   module: {
